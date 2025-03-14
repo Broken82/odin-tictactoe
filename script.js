b4c7d0aec11fe97cac202f8840be19c1dd6c0211
Broken82 <zgrass11@gmail.com>
2025-03-14 21:14:04 +0100

    add basic logic, checking columns and rows

---
+++ script.js
@@ -0,0 +1,96 @@
+function createGameboard(){
+    let board = [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', '']
+    ];
+
+    return {board}
+
+
+}
+
+function createPlayer(name, sign){
+    return {name, sign}
+}
+
+function createGame(gameboard){
+
+    let moveCount = 0
+    
+
+    function play(player){
+        const cords = prompt('Tell me coordinates! x,y: ')
+
+        const [x,y] = cords.split(',');
+
+        if(gameboard.board[x][y] === 'X' || gameboard.board[x][y] === 'O'){
+            return console.log('Its taken!');
+        }
+        gameboard.board[x][y] = player.sign;
+        console.log(`${player.name} has placed ${player.sign}`);
+        moveCount = moveCount + 1;
+
+        //print board
+        console.log(gameboard)
+
+
+        if(moveCount >= 5){
+            checkVictory(player, x, y);
+        }
+
+    }
+
+    function checkVictory(player, x, y){
+
+        console.log('checking victory')
+
+        let gameboardSize = gameboard.board.length;
+
+        console.log(gameboardSize)
+
+        //check columns
+        for(let i = 0; i < gameboardSize; i++){
+            console.log('checking columns...')
+            if(gameboard.board[x][i] != player.sign){
+                break;
+            }
+            if(i == gameboardSize - 1){
+                console.log(`${player.name} has won!`);
+            }
+        }
+
+        //check rows
+        for(let i = 0; i < gameboardSize; i++){
+            console.log('checking rows for ' + player.sign)
+            if(gameboard.board[i][y] != player.sign){
+                break
+            }
+            if(i == gameboardSize - 1){
+                console.log(`${player.name} has won!`);
+            }
+
+        }
+
+        //check diagnolas
+
+
+
+        
+
+
+        
+
+    }
+
+
+
+
+
+        return {play}
+}
+
+const gameBoard = createGameboard();
+const player1 = createPlayer('Szefa', 'X');
+const player2 = createPlayer('Xana', 'O');
+const game = createGame(gameBoard);
